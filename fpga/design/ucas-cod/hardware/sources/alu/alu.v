e2ef2c6b9879e5aabb394e11e68f21753242f31e
Han Chuxiao <hanchuxiao23@mails.ucas.ac.cn>
2025-03-20 09:05:22 +0800

    reconstruction of the alu code, passed the testbench

--- fpga/design/ucas-cod/hardware/sources/alu/alu.v
+++ fpga/design/ucas-cod/hardware/sources/alu/alu.v
@@ -1,40 +1,55 @@
 `timescale 10 ns / 1 ns
-
 `define DATA_WIDTH 32
 
 module alu (
     input  [`DATA_WIDTH - 1:0] A,
     input  [`DATA_WIDTH - 1:0] B,
-    input  [              2:0] ALUop,
-    output                     Overflow,
-    output                     CarryOut,
-    output                     Zero,
+    input  [2:0] ALUop,
+    output Overflow,
+    output CarryOut,
+    output Zero,
     output [`DATA_WIDTH - 1:0] Result
 );
-  // TODO: Please add your logic design here
-  wire [`DATA_WIDTH - 1:0] temp_result;
-  wire [`DATA_WIDTH - 1:0] temp;
 
-  assign Result = ALUop == 3'b000 ? (A & B) : Result;
-  assign Zero = ALUop == 3'b000 ? ((ALUop == 3'b000 ? (A & B) : Result) == {`DATA_WIDTH{0}}) : Zero;
 
-  assign Result = ALUop == 3'b001 ? (A | B) : Result;
-  assign Zero = ALUop == 3'b001 ? ((ALUop == 3'b001 ? (A | B) : Result) == {`DATA_WIDTH{0}}) : Zero;
+wire [`DATA_WIDTH:0] add_result = {1'b0, A} + {1'b0, B};
+wire [`DATA_WIDTH:0] sub_result = {1'b0, A} + {1'b1, ~B} + 1'b1;
 
-  assign {CarryOut, Result} = ALUop == 3'b010 ? (A + B) : {CarryOut, Result};
-  assign Overflow    = ALUop == 3'b010 ? (A[`DATA_WIDTH - 1] == B[`DATA_WIDTH - 1] && A[`DATA_WIDTH - 1] != Result[`DATA_WIDTH - 1]) : Overflow;
-  assign Zero        = ALUop == 3'b010 ? ((ALUop == 3'b010 ? Result : {`DATA_WIDTH{1'bx}}) == {`DATA_WIDTH{0}}) : Zero;
 
-  assign temp = ALUop == 3'b110 ? ~B : temp;
-  assign {CarryOut, Result} = ALUop == 3'b110 ? (A + temp + 1'b1) : {CarryOut, Result};
-  assign Overflow        = ALUop == 3'b110 ? (A[`DATA_WIDTH - 1] != B[`DATA_WIDTH - 1] && A[`DATA_WIDTH - 1] != Result[`DATA_WIDTH - 1]) : Overflow;
-  assign Zero            = ALUop == 3'b110 ? ((ALUop == 3'b110 ? Result : {`DATA_WIDTH{1'bx}}) == {`DATA_WIDTH{0}}) : Zero;
+wire add_overflow = (A[`DATA_WIDTH-1] == B[`DATA_WIDTH-1]) && 
+                   (add_result[`DATA_WIDTH-1] != A[`DATA_WIDTH-1]);
+                   
+wire sub_overflow = (A[`DATA_WIDTH-1] != B[`DATA_WIDTH-1]) && 
+                   (sub_result[`DATA_WIDTH-1] != A[`DATA_WIDTH-1]);
 
-  assign temp = ALUop == 3'b111 ? ~B : temp;
-  assign {CarryOut, temp_result} = ALUop == 3'b111 ? (A + temp + 1'b1) ^ {1, {`DATA_WIDTH{0}}} : {CarryOut, temp_result};
-  assign Overflow          = ALUop == 3'b111 ? (A[`DATA_WIDTH - 1] != B[`DATA_WIDTH - 1] && A[`DATA_WIDTH - 1] != temp_result[`DATA_WIDTH - 1]) : Overflow;
-  assign Zero              = ALUop == 3'b111 ? ((ALUop == 3'b111 ? temp_result[`DATA_WIDTH-1:0] : {`DATA_WIDTH{1'bx}}) == {`DATA_WIDTH{0}}) : Zero;
-  assign Result = ALUop == 3'b111 ? {{(`DATA_WIDTH - 1) {0}}, temp_result[`DATA_WIDTH-1]} : Result;
 
+wire slt_result;
+assign slt_result = 
+    (A[`DATA_WIDTH-1] & !B[`DATA_WIDTH-1]) ? 1'b1 :
+    (!A[`DATA_WIDTH-1] & B[`DATA_WIDTH-1]) ? 1'b0 :
+    (sub_overflow ^ sub_result[`DATA_WIDTH-1]);  
+
+wire sub_carryout = sub_result[`DATA_WIDTH];
+
+assign Result = 
+    (ALUop == 3'b000) ? (A & B) :
+    (ALUop == 3'b001) ? (A | B) :
+    (ALUop == 3'b010) ? (A + B) :
+    (ALUop == 3'b110) ? sub_result[`DATA_WIDTH-1:0] :
+    (ALUop == 3'b111) ? {{(`DATA_WIDTH-1){1'b0}}, slt_result} :
+    {`DATA_WIDTH{1'bx}};
+
+assign Zero = (Result == 0);
+
+assign CarryOut = 
+    (ALUop == 3'b010) ? add_result[`DATA_WIDTH] :
+    (ALUop == 3'b110 | ALUop == 3'b111) ? sub_carryout :
+    1'b0;
+
+assign Overflow = 
+    (ALUop == 3'b010) ? (A[`DATA_WIDTH-1] == B[`DATA_WIDTH-1] && Result[`DATA_WIDTH-1] != A[`DATA_WIDTH-1]) :
+    (ALUop == 3'b110) ? (A[`DATA_WIDTH-1] != B[`DATA_WIDTH-1] && Result[`DATA_WIDTH-1] != A[`DATA_WIDTH-1]) :
+    (ALUop == 3'b111) ? (A[`DATA_WIDTH-1] ^ B[`DATA_WIDTH-1]) & (A[`DATA_WIDTH-1] ^ Result[`DATA_WIDTH-1]) :
+    1'b0;
 
 endmodule
