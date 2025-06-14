2ed0f6ba285f4bd7c2145ff5548bcf6f6a86e581
Han Chuxiao <hanchuxiao23@mails.ucas.ac.cn>
2025-06-14 13:57:55 +0800

    test: fixed grammar issue

--- fpga/design/ucas-cod/hardware/sources/custom_cpu/cache/icache_top.v
+++ fpga/design/ucas-cod/hardware/sources/custom_cpu/cache/icache_top.v
@@ -125,10 +125,16 @@ module icache_top (
     end
   end
 
-  assign way_last_hit = last_hit[index]; // assign the last hit time for the current index
+  genvar i;
+  generate
+      for (i = 0; i < `CACHE_WAY; i = i + 1) begin
+          // For each element 'i' of the way_last_hit array,
+          // assign it the corresponding element from the selected row of the last_hit memory.
+          assign way_last_hit[i] = last_hit[index][i];
+      end
+  endgenerate
 
   // generate cache
-  genvar i;
   generate
     for (i = 0; i < `CACHE_WAY; i = i + 1) begin
       custom_array #(
